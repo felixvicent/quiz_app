514a36a34e5bd504dfdcff4a1b7f9a3edf1a5b39
Felix Vicente <felixvicent1306@gmail.com>
2021-12-29 19:50:52 -0300

    Added questions and button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, deprecated_member_use
 
 import 'package:flutter/material.dart';
 
@@ -9,12 +9,34 @@ void main() {
 class QuestionApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final questions = [
+      'Qual o 5º elemento da sequancia Fibonacci?',
+      'Qual a velocidade da luz',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Hello Felps'),
         ),
-        body: Text('Hello Felps'),
+        body: Column(
+          // ignore: prefer_const_literals_to_create_immutables, deprecated_member_use
+          children: [
+            Text(questions[0]),
+            RaisedButton(
+              child: Text('5'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('8'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
