ed498717bf7605b0952d685c476e71838a7f8b20
Felix Vicente <felixvicent1306@gmail.com>
2021-12-30 13:29:56 -0300

    Avoid to acessa a invalid question

--- lib/main.dart
+++ lib/main.dart
@@ -11,43 +11,52 @@ void main() {
 class _QuestionAppState extends State<QuestionApp> {
   var _selectedAnswer = 0;
 
+  final _questions = const [
+    {
+      'question': 'Qual é o 5º elemento da sequência Fibonacci?',
+      'answer': ['5', '3', '8', '13'],
+    },
+    {
+      'question': 'Qual a velocidade da luz?',
+      'answer': ['200.000 m/s', '50.000 m/s', '300.000 m/s', '120.000 m/s'],
+    },
+    {
+      'question': 'Quantos segundos tem um hora?',
+      'answer': ['3.600', '3.200', '2.800', '4.000'],
+    }
+  ];
+
   void _toRespond() {
-    setState(() {
-      _selectedAnswer++;
-    });
+    if (hasSelectedQuestion) {
+      setState(() {
+        _selectedAnswer++;
+      });
+    }
+  }
+
+  bool get hasSelectedQuestion {
+    return _selectedAnswer < _questions.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> questions = [
-      {
-        'question': 'Qual é o 5º elemento da sequência Fibonacci?',
-        'answer': ['5', '3', '8', '13'],
-      },
-      {
-        'question': 'Qual a velocidade da luz?',
-        'answer': ['200.000 m/s', '50.000 m/s', '300.000 m/s', '120.000 m/s'],
-      },
-      {
-        'question': 'Quantos segundos tem um hora?',
-        'answer': ['3.600', '3.200', '2.800', '4.000'],
-      }
-    ];
-
-    List<String> answers = questions[_selectedAnswer]['answer'] as List<String>;
+    List<String>? answers = hasSelectedQuestion
+        ? _questions[_selectedAnswer]['answer'] as List<String>
+        : null;
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Column(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            Question(questions[_selectedAnswer]['question'] as String),
-            ...answers.map((text) => Answer(text, _toRespond)).toList(),
-          ],
-        ),
+        body: hasSelectedQuestion
+            ? Column(
+                children: [
+                  Question(_questions[_selectedAnswer]['question'] as String),
+                  ...answers!.map((text) => Answer(text, _toRespond)).toList(),
+                ],
+              )
+            : null,
       ),
     );
   }
